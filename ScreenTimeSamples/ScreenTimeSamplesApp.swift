8c9001917a46b072010a9ebb1ebd5bede1290669
Tsubata Seisyu <seisyuu43@gmail.com>
2023-09-24 07:39:19 +0900

    Initial Commit

---
+++ ScreenTimeSamples/ScreenTimeSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeSamplesApp.swift
+//  ScreenTimeSamples
+//
+//  Created by seisyu.tsubata on 2023/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
